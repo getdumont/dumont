b5a97245c467824028b7fe67fb03a88b5cb25222
Guilherme Diego <guidiego.expgames@gmail.com>
2018-04-01 01:03:16 -0300

    Add: Appa interface

--- collector/twitter.js
+++ collector/twitter.js
@@ -1,23 +1,28 @@
 const twitter = require('./client');
 const Tweet = require('./schema/tweet');
 const User = require('./schema/user');
+const Appa = require('./appa');
 
+const concurrency = 10;
 const WORDS = [
     'triste',
     'bad',
     'sad'
 ]
 
+const appa = new Appa();
+appa.registerEntity('Tweet', Tweet);
+appa.registerEntity('User', User);
+
+const registerTweet = tweet => {
+    appa.Tweet.send(tweet);
+    return twitter.getUser(tweet.user.id, tweet.user.screen_name)
+}
 
 twitter
     .getTweets(WORDS.reduce((a, c) => `${a} OR ${c}`))
-    .map(tweet => {
-        console.log(Tweet(tweet));
-        return twitter.getUser(tweet.user.id, tweet.user.screen_name)
-    })
-    .map(user => {
-        console.log(User(user));
-    })
+    .map(registerTweet, { concurrency })
+    .map(appa.User.send, { concurrency })
     .catch(e => {
         console.log(e);
     })
