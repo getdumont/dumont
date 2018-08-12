364989b8db84697afbebe6ae2cbfa00de38b6165
Guilherme Diego <guidiego.expgames@gmail.com>
2018-08-12 16:48:44 -0300

    Fix: Stage one to remove RT

--- tasks/processing/tweets/stage_1.py
+++ tasks/processing/tweets/stage_1.py
@@ -1,18 +1,30 @@
+import re
 from externals.npl import npl
 from processing.utilities.sentiment import get_sentiment_from
 from processing.utilities.text import remove_stopwords, create_tree, join_tokens
 
-def stage_1(doc):
-    rawText = doc['text_object']['clearText']
-    tokens = npl(rawText)
+RT_PATTERN = r'RT \@\w*'
 
+def stage_1(doc):
+    additional_data = {}
+    rawText = doc['text_object']['clearText']
+
+    if re.search(RT_PATTERN, rawText) is not None:
+        rawText = remove_rt_from_text(rawText)
+        additional_data['without_rt'] = rawText
+
+    tokens = npl(rawText)
     cleanTokens = remove_stopwords(tokens)
     cleanText = join_tokens(cleanTokens)
 
     return {
+        **additional_data,
         'clean_text': cleanText,
         'clean_tree': create_tree(cleanTokens),
         'raw_tree': create_tree(tokens),
         'clean_sentiment': get_sentiment_from(cleanText),
         'raw_sentiment': get_sentiment_from(rawText),
     }
+
+def remove_rt_from_text(text):
+    return re.sub(RT_PATTERN, '', text).strip()
